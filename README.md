3b4028e05ec0b784500c57f0a99645b7e414a5b4
milaq <mail@milaq.net>
2023-07-20 12:41:25 +0200

    unify and rework repo architecture

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 [![](http://kodi.wiki/images/4/43/Side-by-side-dark-transparent.png)](https://kodi.tv/)
 
-# milaq's addon repository for Kodi 19 (Matrix)
+# milaq's addon repository for Kodi
 
-Install the <a href="https://github.com/milaq/kodi_repository_milaq/raw/matrix/pool/repository.milaq/repository.milaq-matrix-2.0.0.zip">repository package</a> via `Settings`>`Add-ons`>`Install from zip file`
+[Repository package [repository.milaq-3.0.0.zip]](https://github.com/milaq/kodi_repository_milaq/raw/master/repository.milaq-3.0.0.zip)
+
+Install the latest repository package via `Settings`>`Add-ons`>`Install from zip file`
--- update_repo.sh
+++ update_repo.sh
@@ -1,54 +1,78 @@
 #!/bin/bash
 
 set -e
-mkdir -p tmp/
+set -o pipefail
+
+REPO_NAME=repository.milaq
+REPO_VERSION=3.0.0
 
 function get_addon_data_github {
-  package_name=$1
-  repo_name=$2
-  version=$3
-  mkdir -p pool/$package_name/
-  wget https://github.com/$repo_name/archive/$version.zip -O tmp/addon.zip
-  mv tmp/addon.zip pool/$package_name/$package_name-$version.zip
-  wget https://raw.githubusercontent.com/$repo_name/$version/addon.xml -O tmp/addon.xml
-  cat tmp/addon.xml | tail -n +2 >> addons.xml
+  kodi_version=$1
+  package_name=$2
+  repo_name=$3
+  shift; shift; shift
+  versions=$@
+  mkdir -p dists/$kodi_version/$package_name/
+  for version in $versions; do
+    echo "Getting addon data for $package_name ($version)..."
+    if [[ ! -f dists/$kodi_version/$package_name/$package_name-$version.zip ]]; then
+      wget -q https://github.com/$repo_name/archive/$version.zip -O dists/$kodi_version/$package_name/$package_name-$version.zip
+    fi
+    unzip -p "dists/$kodi_version/$package_name/$package_name-$version.zip" "*/addon.xml" | tail -n +2 >> dists/$kodi_version/addons.xml
+  done
+}
+
+function get_self_repo_data {
+  kodi_version=$1
+  mkdir -p dists/$kodi_version/$REPO_NAME/
+  cp $REPO_NAME-$REPO_VERSION.zip dists/$kodi_version/$REPO_NAME/
+  cat $REPO_NAME/addon.xml | tail -n +2 >> dists/$kodi_version/addons.xml
 }
 
 function gen_xml_header {
-  echo "<?xml version="1.0" encoding="UTF-8" standalone="yes"?>" > addons.xml
-  echo "<addons>" >> addons.xml
+  kodi_version=$1
+  echo "<?xml version=/"1.0/" encoding=/"UTF-8/" standalone=/"yes/"?>" > dists/$kodi_version/addons.xml
+  echo "<addons>" >> dists/$kodi_version/addons.xml
 }
 
 function gen_xml_footer {
-  echo "</addons>" >> addons.xml
+  kodi_version=$1
+  echo "</addons>" >> dists/$kodi_version/addons.xml
 }
 
 function gen_checksum {
-  md5sum addons.xml | awk '{print $1}' > addons.xml.md5
+  kodi_version=$1
+  md5sum dists/$kodi_version/addons.xml | awk '{print $1}' > dists/$kodi_version/addons.xml.md5
 }
 
-function get_repo_data {
-  package_name=$1
-  kodi_version=$2
-  version=$3
-  mkdir -p pool/$package_name/
-  pushd dist
-  zip -o -r ../pool/$package_name/$package_name-$kodi_version-$version.zip $package_name/
-  popd
-  cat dist/$package_name/addon.xml > tmp/addon.xml
-  cat tmp/addon.xml | tail -n +2 >> addons.xml
+function gen_repoarchive {
+  zip -r $REPO_NAME-$REPO_VERSION.zip $REPO_NAME/
 }
 
-gen_xml_header
+echo "Generating repo archive..."
+gen_repoarchive
 
-get_repo_data repository.milaq matrix 2.0.0
-get_addon_data_github service.blackbarsremover milaq/kodi_addon_blackbarsremover 3.1.0
-get_addon_data_github resource.uisounds.nebula.mlq milaq/kodi_uisounds_nebula_mlq 1.0.0
-get_addon_data_github screensaver.fanart.slideshow milaq/kodi_screensaver_fanart_slideshow 0.9.6
-get_addon_data_github service.odroid.screenoff milaq/kodi_addon_odroid_screenoff 1.1.0
+echo "Generating repo data for Kodi Leia..."
+mkdir -p dists/leia
+gen_xml_header leia
+get_self_repo_data leia
+get_addon_data_github leia service.blackbarsremover milaq/kodi_addon_blackbarsremover "2.0.1" "2.1.0" "2.1.1"
+get_addon_data_github leia resource.uisounds.nebula.mlq milaq/kodi_uisounds_nebula_mlq "1.0.0"
+get_addon_data_github leia screensaver.fanart.slideshow milaq/kodi_screensaver_fanart_slideshow "0.9.5" "0.9.6"
+get_addon_data_github leia service.odroid.screenoff milaq/kodi_addon_odroid_screenoff "1.0.0" "1.0.1" "1.1.0"
+gen_xml_footer leia
+gen_checksum leia
 
-gen_xml_footer
-gen_checksum
+echo "Generating repo data for Kodi Matrix..."
+mkdir -p dists/matrix
+gen_xml_header matrix
+get_self_repo_data matrix
+get_addon_data_github matrix service.blackbarsremover milaq/kodi_addon_blackbarsremover "3.0.0" "3.1.0"
+get_addon_data_github matrix resource.uisounds.nebula.mlq milaq/kodi_uisounds_nebula_mlq "1.0.0"
+get_addon_data_github matrix screensaver.fanart.slideshow milaq/kodi_screensaver_fanart_slideshow "0.9.6"
+get_addon_data_github matrix service.odroid.screenoff milaq/kodi_addon_odroid_screenoff "1.1.0"
+gen_xml_footer matrix
+gen_checksum matrix
 
 echo
 echo --------------------------------------
